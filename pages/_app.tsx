1e6c01c0d044c4c60f8205fd26e5d10685914692
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2022-07-20 22:02:14 +0530

    chakra ui theme updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import {ChakraProvider,extendTheme} from "@chakra-ui/react"
+import "reset-css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const theme = extendTheme({
+  colors:{
+    gray:{
+      100:'#f5f5f5',
+      200:'#eeeeee',
+      300:'#eoeoeo',
+      400:'#bdbdbd',
+      500:'#9e9e9e',
+      600:'#757575',
+      700:'#616161',
+      800:'#424242',
+      900:'#212121',
+    },
+  },
+  components:{
+    Button:{
+      variants:{
+        link:{
+          ':focus':{
+            outline:'none',
+            boxShadow:'none',
+          },
+        },
+      },
+    },
+  },
+})
+
+const  MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+       <Component {...pageProps} />
+    </ChakraProvider>
+
+  )
+  
+ 
 }
 
-export default MyApp
+export default MyApp;
